7ac468fa12a6d488d9dd70db76fc431681a3fb60
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-09-22 00:14:31 -0600

    Configured knexfile.js and dbConfig.js

---
+++ knexfile.js
@@ -0,0 +1,46 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/hobbits.db3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  },
+
+  //need to watch videos. May need to change the connection and client values that are set up  for postgress. These may need to be different for first time production setup with heroku
+  //npx heroku run knex migrate:latest -a name_of_heroku api
+  // production: {
+  //   client: 'pg',
+  //   connection: process.env.DATABASE_URL,//
+  //   migrations: {
+  //     directory: './data/migrations',
+  //   },
+  //   seeds: {
+  //     directory: './data/seeds',
+  //   },
+  // },
+
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/test.db3',
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },  
+
+};
