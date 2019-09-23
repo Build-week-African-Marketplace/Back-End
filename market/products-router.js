bb857903ce02249f1ad69e2e29dc204309f6c63b
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-09-22 22:25:37 -0600

    Added helper functions for products-router.js

--- market/products-router.js
+++ market/products-router.js
@@ -3,9 +3,25 @@ const restricted = require('../auth/auth-middleware.js');
 
 Products = require('./products-model.js');
 
+/************** GET USER PRODUCTS **************/
 router.get('/', restricted, (req, res) => {
     res.send("Products are waiting!")
-})
+});
+
+/************** ADD USER PRODUCT **************/
+router.post('/', (req, res) => {
+
+});
+
+/************ UPDATE USER PRODUCT ************/
+router.put('/:id', (req, res) => {
+    
+});
+
+/*********** DELETE USER PRODUCT ***********/
+router.delete('/:id', (req, res) => {
+    
+});
 
 
 
