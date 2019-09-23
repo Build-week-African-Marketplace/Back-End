0161a88f787eadf2706ee104e2ad6a0a842f0468
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-09-22 19:45:28 -0600

    Created products table  migration

--- database/migrations/20190922162929_bootstrap.js
+++ database/migrations/20190922162929_bootstrap.js
@@ -30,8 +30,42 @@ exports.up = function (knex) {
                 .string('first_name', 156)
                 .unique();
         })
+
+        .createTable('products', products => {
+            //id
+            products.increments()
+
+            //name --> string --> notNullable()
+            products
+                .string('name', 156)
+                .notNullable()
+
+            //description --> string
+            products
+                .string('description', 255)
+
+            //price --> integer (or decimal) --> notNullable()
+            // specificType(name, type) = Sets a specific type for the column creation, if you'd like to add a column type that isn't supported here.
+            products
+                .integer()
+                .notNullable()
+
+
+
+            //user_id FK
+            products
+                .integer('users_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('users')
+                .onDelete('CASCADE')
+                .onUpdate('CASCADE');
+        })
 };
 
 exports.down = function (knex) {
-    return knex.schema.dropTableIfExists('users');
+    return knex.schema
+        .dropTableIfExists('users')
+        .dropTableIfExists('products')
 };
