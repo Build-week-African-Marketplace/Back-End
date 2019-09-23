9f4cd4567b64a281230aa4a67ed38c53e1f323df
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-09-23 14:39:06 -0600

    Added postgress dependency and refactored some knex code

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,6 +1,8 @@
 const knex = require('knex');
 const config = require('../knexfile.js');
 
-const environment = process.env.DB_ENV || 'development';
+const secrets = require('../config/secrets')
+
+const environment = secrets.environment|| 'development';
 
 module.exports = knex(config[environment]);
